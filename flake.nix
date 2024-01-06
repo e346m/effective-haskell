cca8059cc58fdc10ff9ac010a3845904ce50ee5f
Eiji Mishiro <eiji346g@gmail.com>
2024-01-06 16:01:01 +0900

    add ghci -v 9.4.2

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Effective Haskell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ haskell.compiler.native-bignum.ghc942 ];
+        };
+      };
+    };
+}
